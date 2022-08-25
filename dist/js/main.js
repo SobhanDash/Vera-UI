154113000c770d5dc669ffe90b8f7b53c2464579
SobhanDash <sobhan7dash@gmail.com>
2022-08-25 18:36:45 +0530

    Modal Done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,8 @@
-const inputs = document.querySelectorAll('.form-control input');
-const labels = document.querySelectorAll('.form-control label');
 var modal = document.querySelector('.modal');
 var modalButton = document.getElementById('modal-button');
 var modalClose = document.querySelector('.modal-close');
 var modalCancel = document.querySelector('#cancel');
 var modalDelete = document.querySelector('#delete');
-
-//Handling input animation
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split('')
-    .map(
-      (letter, idx) => `<span style="
-				transition-delay: ${idx * 50}ms
-			">${letter}</span>`
-    )
-    .join('');
-});
 
 //Handling modal functionality
 modalButton.onclick = function () {
